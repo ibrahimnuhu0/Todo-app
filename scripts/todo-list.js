699c7fee35ce3d4f9222c50bc4469f6a4d35695e
Ibrahim Nuhu <ibrahimnuhu0@gmail.com>
2025-05-04 23:19:51 +0600

    Making Todo list interractive with HTML

---
+++ scripts/todo-list.js
@@ -0,0 +1,43 @@
+const todoList = [
+  ];
+
+renderTodoList();
+
+function renderTodoList () {
+
+let todoRender = '';
+
+for (i = 0; i < todoList.length; i++){
+const todo = todoList[i];
+const {name, dueDate} = todo;
+
+const html = `<p>${name} ${dueDate}<button onclick="
+todoList.splice(${i}, 1);
+renderTodoList();
+">Delete</button> </p>`;
+todoRender += html;
+}
+
+console.log(todoRender);
+
+document.querySelector('.js-todo-list').innerHTML = todoRender;
+
+}
+
+function addTodo () {
+const inputElement =  document.querySelector('.js-name-input');
+const name =  inputElement.value;
+
+const dateInputElement = document.querySelector('.js-due-date-input');
+
+const dueDate = dateInputElement.value;
+
+todoList.push({name,
+  dueDate
+});
+
+
+inputElement.value = '';
+
+renderTodoList();
+}
